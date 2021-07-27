b1eac0c725ffd5aa7e0e678ea046c8cee583b798
Ahmed Mohammed <eng.a7medfci@gmail.com>
2021-07-27 21:45:27 +0200

    add seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(StoreSeeder::class);
         // \App\Models\User::factory(10)->create();
     }
 }
